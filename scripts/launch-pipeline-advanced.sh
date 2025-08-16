e02afdc12eb9b273464592b1946b9bf90ca591cd
Alexandre Fenyo <alexandre.fenyo@gmail.com>
2025-08-16 23:51:34 +0200

    new

--- scripts/launch-pipeline-advanced.sh
+++ scripts/launch-pipeline-advanced.sh
@@ -8,21 +8,20 @@ echo stopping Weaviate:
 echo starting Weaviate:
 ./scripts/start-weaviate.sh
 
-#echo converting document:
-#./src/pipeline/convert_to_markdown.py $HOME/CCTP/CCTP2.docx
+echo converting document:
+./src/pipeline/convert_to_markdown.py $HOME/CCTP/CCTP2.docx
 
-#echo creating chunks:
-#./src/pipeline-advanced/create_chunks.py $HOME/CCTP/CCTP2.docx.md
+echo creating chunks:
+./src/pipeline-advanced/create_chunks.py $HOME/CCTP/CCTP2.docx.md
 
 echo creating embeddings:
+rm -f $HOME/CCTP/CCTP2.docx.md.chunks.jq.paraphrase-xlm-r-multilingual-v1.emb_cache.jsonl
 ./src/pipeline-advanced/create_embeddings.py $HOME/CCTP/CCTP2.docx.md.chunks.jq
 
-exit 0
-
 echo updating Weaviate:
-./src/pipeline/update_weaviate.py $HOME/CCTP/CCTP2.docx.md.chunks.jq.embeddings.ndjson
+./src/pipeline-advanced/update_weaviate.py $HOME/CCTP/CCTP2.docx.md.chunks.jq.embeddings.ndjson
 
 echo making request:
-./src/pipeline/search_chunks.py "Les CPAM sont-elles publiques ou privées ?"
+./src/pipeline-advanced/search_chunks.py "Les CPAM sont-elles publiques ou privées ?"
 
 date
